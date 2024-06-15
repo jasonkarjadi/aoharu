12df179a505e7017afa6e2ccf1670528d38180d4
jason@karjadi.com <jason@karjadi.com>
2024-06-16 04:14:30 +0800

    add round button component

---
+++ src/routes/LocaleBtn.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let locale;
+</script>
+
+<a
+  href="/{locale.toLowerCase()}"
+  class="rounded-full border-2 text-center flex flex-col justify-center items-center w-24 h-24 text-white bg-slate-500 border-white"
+>
+  <h2 class="text-xl"><slot /></h2>
+  <span class="text-xs">{locale.toUpperCase()}</span>
+</a>
