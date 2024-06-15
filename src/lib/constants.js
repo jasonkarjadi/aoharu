350243844d64cf8083a9aaf98a142e2a9eecba99
jason@karjadi.com <jason@karjadi.com>
2024-06-16 04:15:18 +0800

    add days constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,3 +3,5 @@ export const mmdds = Array(12)
   .fill()
   .map((_, idx) => [++idx, idx === 2 ? 28 : 30 + days31.includes(idx)]);
 export const leapmmdds = mmdds.with(1, [2, 29]);
+
+// find shorter way to write using new Date() from 01/01 to 12/31 for year
