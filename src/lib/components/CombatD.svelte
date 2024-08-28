36e9ac42c43f09759f8c6796f87e37de05bf2138
jason@karjadi.com <jason@karjadi.com>
2024-08-28 21:07:21 +0800

    move from local postgres to supabase

--- src/lib/components/CombatD.svelte
+++ src/lib/components/CombatD.svelte
@@ -1,29 +1,20 @@
 <script>
   export let type;
-  export let text;
 </script>
 
-<td class="border text-neutral-50 font-medium {type}">{text}</td>
+<td class="border text-neutral-50 font-medium {type.id}">{type.name}</td>
 
 <style>
-  .爆発,
-  .軽装備 {
-    /* background-color: #930007; */
-    background-color: #920106;
+  .\31 {
+    background-color: #86000b;
   }
-  .貫通,
-  .重装甲 {
-    /* background-color: #bb8800; */
-    background-color: #bd8902;
+  .\32 {
+    background-color: #bb7d10;
   }
-  .神秘,
-  .特殊装甲 {
-    /* background-color: #1f6095; */
-    background-color: #226f9b;
+  .\33 {
+    background-color: #1f6095;
   }
-  .振動,
-  .弾力装甲 {
-    /* background-color: #9743a3; */
-    background-color: #9a46a8;
+  .\34 {
+    background-color: #9743a3;
   }
 </style>
--- src/routes/[locale]/students/+page.svelte
+++ src/routes/[locale]/students/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import { page } from "$app/stores";
   import CombatD from "$lib/components/CombatD.svelte";
-  import TerrainD from "$lib/components/TerrainD.svelte";
+  // import TerrainD from "$lib/components/TerrainD.svelte";
 
   export let data;
-  let { rows } = data;
+  let { students, error } = data;
+  $: error && console.error(error);
 </script>
 
 <div class="mx-4">
@@ -38,94 +39,73 @@
         </tr>
       </thead>
       <tbody class="text-center">
-        {#each rows as unit (unit.name + unit.outfit)}
+        {#each students as student (student.id)}
+          {@const { student_profile, student_outfit } = student}
+          {@const { student_name, student_surname, type_weapon } =
+            student_profile}
           <tr class="*:border-black even:bg-slate-200">
             <td class="border-y p-0">
               <div class="flex gap-2 items-center">
                 <div class="bg-slate-500 size-6"><img src="#" alt="" /></div>
                 <div class="flex-1 text-right leading-none">
                   <ruby class="text-[10px] font-bold">
-                    {unit.surname_kanji}<rt>{unit.surname_kana}</rt>
+                    {#if $page.params.locale === "jpn"}
+                      {student_surname.name.kanji}<rt
+                        >{student_surname.name.kana}</rt
+                      >
+                    {:else}
+                      {student_surname.name}
+                    {/if}
                   </ruby>
                 </div>
               </div>
             </td>
             <td class="border-y p-0 pr-2">
               <div class="flex justify-between gap-2">
-                <span>{unit.name}</span>
+                <span>{student_name.name}</span>
                 <span class="text-[8px] font-bold">
-                  {unit.outfit}
+                  {student_outfit.name}
                 </span>
               </div>
             </td>
-            <td class="px-2 border">{unit.school}</td>
-            <td class="px-2 border">{unit.club}</td>
-            <td class="px-2 border font-bold {unit.role}">
-              {unit.role}
+            <td class="px-2 border">{student_profile.school.name}</td>
+            <td class="px-2 border">{student_profile.school_club.name}</td>
+            <td class="px-2 border font-bold {student.combat_role.name}">
+              {student.combat_role.name}
             </td>
-            <CombatD
-              type={unit.attack_type}
-              text={unit[`attack_type_${$page.params.locale}`]}
-            />
-            <CombatD
-              type={unit.defense_type}
-              text={unit[`defense_type_${$page.params.locale}`]}
-            />
-            <td class="px-2 border">{unit.class}</td>
-            <td class="border">{unit.weapon_type}</td>
-            <td class="border">{unit.cover ? "○" : "×"}</td>
+            <CombatD type={student.type_attack} />
+            <CombatD type={student.type_defense} />
+            <td class="px-2 border">{student.combat_class.name}</td>
+            <td class="border">{type_weapon.name}</td>
+            <td class="border">{student.cover ? "○" : "×"}</td>
             <td
               class="border italic font-medium text-neutral-50 bg-[rgb(43,68,101)]"
             >
-              {unit.position}
+              {type_weapon.combat_position.name}
             </td>
-            <TerrainD affinity={unit.city_affinity} />
-            <TerrainD affinity={unit.field_affinity} />
-            <TerrainD affinity={unit.indoor_affinity} />
-            <td class="border">EX</td>
-            <td class="border">NS</td>
-            <td class="border">SS</td>
-            <td class="w-24 border">{unit.equipment_first}</td>
-            <td class="w-24 border">{unit.equipment_second}</td>
-            <td class="w-24 border">{unit.equipment_third}</td>
-            <td class="border">{unit.recruit}</td>
+            <!-- <TerrainD affinity={student.city_affinity} />
+            <TerrainD affinity={student.field_affinity} />
+            <TerrainD affinity={student.indoor_affinity} /> -->
+            <td class="border URBAN"></td>
+            <td class="border OUTDOOR"></td>
+            <td class="border INDOOR"></td>
+            <td class="border EX"></td>
+            <td class="border NS"></td>
+            <td class="border SS"></td>
+            <td class="w-24 border">{student.equipment_first.name}</td>
+            <td class="w-24 border">{student.equipment_second.name}</td>
+            <td class="w-24 border">{student.equipment_third.name}</td>
+            <td class="border">{student.type_recruitment.name}</td>
             <td class="px-2 border">
-              {unit.release_date
-                ?.toISOString()
-                .slice(0, 10)
-                .replaceAll("-", "/")}
+              {student.release_date_jpn}
             </td>
-            <td class="border">exgift</td>
-            <td class="border">gift</td>
+            <td class="border exgift"></td>
+            <td class="border gift"></td>
           </tr>
         {/each}
       </tbody>
     </table>
   </div>
-  <!-- <div class="fixed bottom-0 left-0 right-0 h-16 border">
-    <div class="flex">
-      <div class="flex">
-        <div class="striker" id="striker_one">
-          <img src="" alt="striker1" />
-        </div>
-        <div class="striker" id="striker_two">
-          <img src="" alt="striker2" />
-        </div>
-        <div class="striker" id="striker_three">
-          <img src="" alt="striker3" />
-        </div>
-        <div class="striker" id="striker_four">
-          <img src="" alt="striker4" />
-        </div>
-        <div class="special" id="special_one">
-          <img src="" alt="special1" />
-        </div>
-        <div class="special" id="special_two">
-          <img src="" alt="special2" />
-        </div>
-      </div>
-    </div>
-  </div> -->
 </div>
 
 <style>
